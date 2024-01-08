600628fa0feec455d8462bd8848c766084f3e500
OleksanderMelnik <alekandrmelnik@gmail.com>
2024-01-08 09:17:17 +0200

    typescript homework 4

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 export function FormComponent() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState<string>("");
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
   return (
-    <input type="text" value={value} onChange={handleChange} />
+    <input type="text" placeholder="text" value={value} onChange={handleChange} />
   );
 }
